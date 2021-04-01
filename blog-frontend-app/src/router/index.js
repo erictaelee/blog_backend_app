6353e3f911c625bdbc851c315fa65764db40113a
Eric <lee.erictae@gmail.com>
2021-03-31 20:34:38 -0500

    add logout route and view

--- blog-frontend-app/src/router/index.js
+++ blog-frontend-app/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Sample from '../views/Sample.vue'
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter)
 
@@ -23,7 +24,14 @@ const routes = [
     path: "/signup",
     name: "signup",
     component: Signup
-  }
+  },
+
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+
 
   {
     path: '/about',
--- blog-frontend-app/src/views/Home.vue
+++ blog-frontend-app/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <a href="/signup">Signup</a> or 
+<router-link to="/signup">Signup</router-link>
+<p><a href="/login">Login</a> or 
+<router-link to="/login">Login</router-link></p>
   </div>
 </template>
 
